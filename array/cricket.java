e412c2b4e8393c087aed59153cac4c72f39e4bdd
tiwarivarun28 <in.tiwarivarun@gmail.com>
2022-11-09 23:37:57 +0530

    all file

--- array/cricket.java
+++ array/cricket.java
@@ -1,3 +1,4 @@
+package array;
 import java.io.*;
 import java.util.ArrayList;
 public class cricket {
--- array/integerarraylist.java
+++ array/integerarraylist.java
@@ -1,3 +1,4 @@
+package array;
 import java.io.*;
 import java.util.*;
 public class integerarraylist {
